a95102f75569c6993ba7fd89a9d830c151ac6d54
Timures <bisemsk@gmail.com>
2024-09-29 17:00:48 +0500

    add sidebar

--- app/(cabinet)/_components/Sidebar.tsx
+++ app/(cabinet)/_components/Sidebar.tsx
@@ -1,7 +1,55 @@
-export default function Sidebar() {
+"use client";
+
+import Link from "next/link";
+import { Plus } from "lucide-react";
+import { useLocalStorage } from "usehooks-ts";
+import { useProfile } from "@/hooks/useProfile";
+
+import { Button } from "@/components/ui/button";
+import { Separator } from "@/components/ui/separator";
+import { Skeleton } from "@/components/ui/skeleton";
+import { Accordion } from "@/components/ui/accordion";
+import { CreateOrganization } from "../organization/CreateOrganization";
+
+interface SidebarProps {
+  storageKey?: string;
+}
+
+const Sidebar = ({ storageKey = "t-sidebar-state" }: SidebarProps) => {
+  const { data, isLoading } = useProfile();
+  const [expanded, setExpanded] = useLocalStorage<Record<string, any>>(
+    storageKey,
+    {}
+  );
+  const defaultAccordionValue: string[] = Object.keys(expanded).reduce(
+    (acc: string[], key: string) => {
+      if (expanded[key]) {
+        acc.push(key);
+      }
+      return acc;
+    },
+    []
+  );
+
+  const onExpand = (id: string) => {
+    setExpanded((current) => ({
+      ...current,
+      [id]: !expanded[id],
+    }));
+  };
+
+  if (isLoading) {
+    return (
+      <>
+        <Skeleton />
+      </>
+    );
+  }
   return (
-    <aside>
-      <h2>Sidebar</h2>
+    <aside className="font-medium text-xs flex items-center mb-1">
+      <span className="pl-4">Workspaces</span>
+      <CreateOrganization />
     </aside>
   );
-}
+};
+export default Sidebar;
